a98133854b7312f482153ecb60c430145a72c0aa
MansurSarkhanov <sarxanov.mansur@gmail.com>
2024-05-10 23:47:26 +0400

    start to write colors

---
+++ digi_task/lib/core/constants/theme/app_colors.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  //primary colors
+  Color get primaryColor0;
+  Color get primaryColor10;
+  Color get primaryColor20;
+  Color get primaryColor30;
+  Color get primaryColor40;
+  Color get primaryColor50;
+  Color get primaryColor60;
+  Color get primaryColor70;
+  Color get primaryColor80;
+  Color get primaryColor90;
+  Color get primaryColor95;
+  Color get primaryColor99;
+  Color get primaryColor100;
+
+  //secondary colors
+  Color get secondaryColor0;
+  Color get secondaryColor10;
+  Color get secondaryColor20;
+  Color get secondaryColor30;
+  Color get secondaryColor40;
+  Color get secondaryColor50;
+  Color get secondaryColor60;
+  Color get secondaryColor70;
+  Color get secondaryColor80;
+  Color get secondaryColor90;
+  Color get secondaryColor95;
+  Color get secondaryColor99;
+  Color get secondaryColor100;
+
+  //success colors
+  Color get successColor0;
+  Color get successColor10;
+  Color get successColor20;
+  Color get successColor30;
+  Color get successColor40;
+  Color get successColor50;
+  Color get successColor60;
+  Color get successColor70;
+  Color get successColor80;
+  Color get successColor90;
+  Color get successColor95;
+  Color get successColor99;
+  Color get successColor100;
+
+  //error colors
+  Color get errorColor0;
+  Color get errorColor10;
+  Color get errorColor20;
+  Color get errorColor30;
+  Color get errorColor40;
+  Color get errorColor50;
+  Color get errorColor60;
+  Color get errorColor70;
+  Color get errorColor80;
+  Color get errorColor90;
+  Color get errorColor95;
+  Color get errorColor99;
+  Color get errorColor100;
+
+  //neutral colors
+  Color get neutralColor0;
+  Color get neutralColor10;
+  Color get neutralColor20;
+  Color get neutralColor30;
+  Color get neutralColor40;
+  Color get neutralColor50;
+  Color get neutralColor60;
+  Color get neutralColor70;
+  Color get neutralColor80;
+  Color get neutralColor90;
+  Color get neutralColor95;
+  Color get neutralColor99;
+  Color get neutralColor100;
+
+  //background color
+  Color get backgroundColor;
+}
---
+++ digi_task/lib/core/constants/theme/theme_colors.dart
@@ -0,0 +1,203 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+class AppLightColors extends AppColors {
+  @override
+  Color get primaryColor0 => const Color(0xFF000000);
+
+  @override
+  Color get primaryColor10 => const Color(0xFF053973);
+
+  @override
+  Color get primaryColor20 => const Color(0xFF064388);
+
+  @override
+  Color get primaryColor30 => const Color(0xFF064A96);
+
+  @override
+  Color get primaryColor40 => const Color(0xFF014FA7);
+
+  @override
+  Color get primaryColor50 => const Color(0xFF005ABF);
+
+  @override
+  Color get primaryColor60 => const Color(0xFF2B75CC);
+
+  @override
+  Color get primaryColor70 => const Color(0xFF5B96DB);
+
+  @override
+  Color get primaryColor80 => const Color(0xFF8BB8EC);
+
+  @override
+  Color get primaryColor90 => const Color(0xFFAED0F5);
+
+  @override
+  Color get primaryColor95 => const Color(0xFFD2E5FA);
+
+  @override
+  Color get primaryColor99 => const Color(0xFFE4EFFB);
+
+  @override
+  Color get primaryColor100 => const Color(0xFFFFFFFF);
+
+  @override
+  Color get backgroundColor => const Color(0xFFF7F5FF);
+
+  @override
+  Color get errorColor0 => const Color(0xFF000000);
+
+  @override
+  Color get errorColor10 => const Color(0xFF750606);
+
+  @override
+  Color get errorColor20 => const Color(0xFF940707);
+
+  @override
+  Color get errorColor30 => const Color(0xFFBB0707);
+
+  @override
+  Color get errorColor40 => const Color(0xFFD60A0A);
+
+  @override
+  Color get errorColor50 => const Color(0xFFEA0A0A);
+
+  @override
+  Color get errorColor60 => const Color(0xFFFF5449);
+
+  @override
+  Color get errorColor70 => const Color(0xFFFF897D);
+
+  @override
+  Color get errorColor80 => const Color(0xFFFFB4AB);
+
+  @override
+  Color get errorColor90 => const Color(0xFFFFDAD6);
+
+  @override
+  Color get errorColor95 => const Color(0xFFFFEDEA);
+
+  @override
+  Color get errorColor99 => const Color(0xFFFFFBFF);
+
+  @override
+  Color get errorColor100 => const Color(0xFFFFFFFF);
+
+  @override
+  Color get neutralColor0 => const Color(0xFF000000);
+
+  @override
+  Color get neutralColor10 => const Color(0xFF000000);
+
+  @override
+  Color get neutralColor20 => const Color(0xFF2F3033);
+
+  @override
+  Color get neutralColor30 => const Color(0xFF2F3033);
+
+  @override
+  Color get neutralColor40 => const Color(0xFF5E5E62);
+
+  @override
+  Color get neutralColor50 => const Color(0xFF76777A);
+
+  @override
+  Color get neutralColor60 => const Color(0xFF909094);
+
+  @override
+  Color get neutralColor70 => const Color(0xFFABABAF);
+
+  @override
+  Color get neutralColor80 => const Color(0xFFC7C6CA);
+
+  @override
+  Color get neutralColor90 => const Color(0xFFE3E2E6);
+
+  @override
+  Color get neutralColor95 => const Color(0xFFF1F0F4);
+
+  @override
+  Color get neutralColor99 => const Color(0xFFFDFBFF);
+
+  @override
+  Color get neutralColor100 => const Color(0xFFFFFFFF);
+
+  @override
+  Color get secondaryColor0 => const Color(0xFF000000);
+
+  @override
+  Color get secondaryColor10 => const Color(0xFF705F07);
+
+  @override
+  Color get secondaryColor20 => const Color(0xFFB49807);
+
+  @override
+  Color get secondaryColor30 => const Color(0xFFC9AA08);
+
+  @override
+  Color get secondaryColor40 => const Color(0xFFE5C208);
+
+  @override
+  Color get secondaryColor50 => const Color(0xFFF8D004);
+
+  @override
+  Color get secondaryColor60 => const Color(0xFFF8D004);
+
+  @override
+  Color get secondaryColor70 => const Color(0xFFFFDB20);
+
+  @override
+  Color get secondaryColor80 => const Color(0xFFFFE03E);
+
+  @override
+  Color get secondaryColor90 => const Color(0xFFFFE76A);
+
+  @override
+  Color get secondaryColor95 => const Color(0xFFFFF0A1);
+
+  @override
+  Color get secondaryColor99 => const Color(0xFFFFF7CC);
+
+  @override
+  Color get secondaryColor100 => const Color(0xFFFFFFFF);
+
+  @override
+  Color get successColor0 => const Color(0xFF000000);
+
+  @override
+  Color get successColor10 => throw UnimplementedError();
+
+  @override
+  Color get successColor20 => throw UnimplementedError();
+
+  @override
+  Color get successColor30 => throw UnimplementedError();
+
+  @override
+  Color get successColor40 => throw UnimplementedError();
+
+  @override
+  Color get successColor50 => throw UnimplementedError();
+
+  @override
+  Color get successColor60 => throw UnimplementedError();
+
+  @override
+  Color get successColor70 => throw UnimplementedError();
+
+  @override
+  Color get successColor80 => throw UnimplementedError();
+
+  @override
+  Color get successColor90 => throw UnimplementedError();
+
+  @override
+  Color get successColor95 => throw UnimplementedError();
+
+  @override
+  Color get successColor99 => throw UnimplementedError();
+
+  @override
+  Color get successColor100 => throw UnimplementedError();
+}
